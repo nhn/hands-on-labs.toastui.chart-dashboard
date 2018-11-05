81d66cff09802818c4214c21a550519b1af5ed0a
김진우 <jw.kim@nhnent.com>
2018-11-05 16:51:53 +0900

    fixed review

--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,44 @@
-const path = require('path');
-const webpack = require('webpack');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = (env, options = {}) => {
-  const config = {
-    entry: {
-      app: ['babel-polyfill', './chart.js']
-    },
-    output: {
-      filename: '[name].bundle.js',
-      libraryTarget: 'umd'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader'
-          }
-        },
-        {
-          test: /\.(png|jpg|gif|ttf)$/,
-          use: {
-            loader: 'file-loader',
-            options: {
-              name: '[path][name].[ext]'
-            }
-          }
-        },
-        {
-          test: /\.css$/,
-          loaders: ['style-loader', 'css-loader']
-        },
-        {
-          test: /\.html$/,
-          loader: "raw-loader"
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: ['babel-polyfill', './src/index.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
         }
-      ]
-    }
-  };
-
-  if (options.mode === 'development') {
-    config.plugins = [
-      new webpack.HotModuleReplacementPlugin(),
-      new HtmlWebpackPlugin({
-          template: './index.html'
-      })
-    ];
-
-    config.devtool = 'inline-source-map';
-    config.devServer = {
-      hot: true,
-      host: '0.0.0.0',
-      contentBase: './dist',
-      watchContentBase: true,
-      stats: {
-        color: true
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          failOnError: false
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
       }
-    };
-  } else {
-    config.plugins = [new CleanWebpackPlugin(['dist'])];
-  }
-
-  config.plugins.push(
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(options.mode)
-      }
-    })
-  );
-
-  return config;
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin(['dist']),
+    new MiniCssExtractPlugin()
+  ],
+  devtool: 'source-map'
 };
-
