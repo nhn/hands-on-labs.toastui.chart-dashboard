3ba28609f50f9f31cf4404ab67fb9616ff4f2188
김진우 <jw.kim@nhnent.com>
2018-11-06 10:45:06 +0900

    add webpack config for ie8

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const es3ifyPlugin = require('es3ify-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-3-webpack-plugin");
 
 module.exports = {
   mode: 'development',
@@ -47,5 +48,14 @@ module.exports = {
     new MiniCssExtractPlugin(),
     new es3ifyPlugin()
   ],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          screw_ie8: false
+        }
+      })
+    ]
+  },
   devtool: 'source-map'
 };
