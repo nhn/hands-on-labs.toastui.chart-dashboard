c838f35283486802ccae63e1f70e13b4e9589770
cybaek <cybaek@nhnent.com>
2018-11-18 00:26:22 +0900

    파일 정리, 디자인 수정.

--- site/conf.py
+++ site/conf.py
@@ -349,3 +349,6 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #
 # texinfo_no_detailmenu = False
+
+def setup(app):
+    app.add_stylesheet('css/custom.css')
